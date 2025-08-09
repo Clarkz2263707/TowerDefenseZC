f9c2ce16e32017d314ee69b387d2f35c433da5e4
Clarkz2263707 <zachary.clark4@g.austincc.edu>
2025-08-08 20:14:44 -0500

    added moneydrop serialization and moneymanager oriented code to be able to make enmies drop money upon death

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,20 +4,30 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-   private NavMeshAgent agent;
+    private NavMeshAgent agent;
     private Animator animator;
     [SerializeField] private Transform EndPoint;
     [SerializeField] private string animatorParam_Iswalking;
     [SerializeField] private int damage;
+    [SerializeField] private int moneyDropped = 10; 
+
+    private Health health;
+
+    public event System.Action<Enemy> OnEnemyDeath;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        health = GetComponent<Health>();
     }
     void Start()
     {
         animator.SetBool(animatorParam_Iswalking, true);
+        if (health != null)
+        {
+            health.onHealthChanged += CheckEnemyDeath;
+        }
     }
 
     public void Initialized(Transform inputEndPoint)
@@ -26,7 +36,6 @@ public class Enemy : MonoBehaviour
         agent.SetDestination(inputEndPoint.position);
     }
 
-   
     void Update()
     {
         if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
@@ -42,6 +51,20 @@ public class Enemy : MonoBehaviour
     {
         animator.SetBool(animatorParam_Iswalking, false);
         GameManager.Instance.playerHealth.TakeDamage(damage);
+        Destroy(gameObject);
+    }
+    void CheckEnemyDeath(int currentHealth, int maxHealth)
+    {
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        MoneyManager.Instance?.AddMoney(moneyDropped);
+        OnEnemyDeath?.Invoke(this);
         Destroy(gameObject);
     }
 }
