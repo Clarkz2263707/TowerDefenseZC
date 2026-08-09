8f6220482c6f771b9e89d40432e2400f89461212
Clarkz2263707 <zachary.clark4@g.austincc.edu>
2026-08-08 23:33:12 -0500

    Updated to play a sound when the catapult is fired, overall handles catapult weapon firing.

--- Assets/Scripts/Tower/Catapult Tower.cs
+++ Assets/Scripts/Tower/Catapult Tower.cs
@@ -2,22 +2,30 @@ using UnityEngine;
 
 public class CatapultTower : Tower
 {
+    //summary
+    //handles the firing operations of the catapult tower
+    //summary
+
     [SerializeField] private CatapultProjectile catapultProjectilePrefab;
+    [SerializeField] private AudioClip CatapultShoot;
 
     protected override void Update()
     {
         base.Update();
     }
 
+    //handles the firing operations of the catapult tower. plays a sound when the projectile is actually fired.
     protected override void FireAt(Enemy target)
     {
         if (catapultProjectilePrefab != null && target != null && weaponTransform != null)
         {
             CatapultProjectile projectile = Instantiate(catapultProjectilePrefab, weaponTransform.position, Quaternion.identity);
             projectile.SetTarget(target.transform);
+            SoundManager.instance.PlaySoundFXClip(CatapultShoot, transform, 1f);
+
         }
     }
-
+    //handles enemy targetting
     protected override Enemy TargetEnemy
     {
         get
