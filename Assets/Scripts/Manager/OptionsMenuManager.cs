cce161d8c0367af438e8d13a4163b902e273af41
Clarkz2263707 <zachary.clark4@g.austincc.edu>
2026-08-09 00:07:22 -0500

    added summary text

--- Assets/Scripts/Manager/OptionsMenuManager.cs
+++ Assets/Scripts/Manager/OptionsMenuManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//summary
+//handles the main menu button that takes you back to the main menu
+//summary
 public class OptionsMenuManager : MonoBehaviour
 {
     [SerializeField] private string mainMenuSceneName = "MainMenu";
