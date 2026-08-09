d9b2f15d073bfaad0f1a1742ef47b9edf4a57c78
Clarkz2263707 <zachary.clark4@g.austincc.edu>
2026-08-08 23:25:14 -0500

    Added summary and normal comments that explain how the ballista tower operates and fires.

--- Assets/Scripts/Tower/Ballista Tower.cs
+++ Assets/Scripts/Tower/Ballista Tower.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class BallistaTower : Tower
 {
+    //summary
+    //handles the firing operations of the ballista tower
+    //summary
     [SerializeField] private Projectile arrowPrefab;
-    
+    [SerializeField] private AudioClip BallistaShoot;
     
 
     protected override void Update()
@@ -11,16 +14,17 @@ public class BallistaTower : Tower
         base.Update();
         
     }
-
+    //Shoots ballista prefab projectile at the set target at the time and plays a sound when it does so.
     protected override void FireAt(Enemy target)
     {
         if (arrowPrefab != null)
         {
             GameObject arrowInstance = Instantiate(arrowPrefab.gameObject, weaponTransform.position, Quaternion.identity);
             arrowInstance.GetComponent<Projectile>().SetTarget(target.transform);
+            SoundManager.instance.PlaySoundFXClip(BallistaShoot, transform, 1f);
         }
     }
-
+    //when an enemy dies it gets the next enemy in its radius.
     protected override Enemy TargetEnemy
     {
         get
