4837ef26720409996fb27569c50fa75c1b15b7fd
Clarkz2263707 <zachary.clark4@g.austincc.edu>
2026-08-09 00:08:35 -0500

    added summary comment that explains what this script does.

--- Assets/Scripts/Manager/VictoryManager.cs
+++ Assets/Scripts/Manager/VictoryManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//summary
+//much like the other menus it handles the main menu buttopn to take you back to the main menu.
+//summary
 public class VictoryManager : MonoBehaviour
 {
     [SerializeField] private string mainMenuSceneName = "MainMenu";
