a2db68b2d4a6465b83916a5b513804f7819f2de8
Clarkz2263707 <zachary.clark4@g.austincc.edu>
2026-08-08 23:30:27 -0500

    Handles the firing operations of the cannon tower. updated it to play a sound when firing

--- Assets/Scripts/Tower/Cannon Tower.cs
+++ Assets/Scripts/Tower/Cannon Tower.cs
@@ -2,26 +2,32 @@ using UnityEngine;
 
 public class CannonTower : Tower
 {
+    //Summary
+    //Handles the firing operations of the cannon tower
+    //Summary
+
     [SerializeField] private GameObject cannonballPrefab;
+    [SerializeField] private AudioClip CannonShoot;
 
     protected override void Update()
     {
         base.Update();
     }
-
+    //handles firing operations of the tower by getting enemy location, instantiating cannonball prefab, firing it at the enemy transform, playing a sfx.
     protected override void FireAt(Enemy target)
     {
         if (cannonballPrefab != null && target != null && weaponTransform != null)
         {
             GameObject cannonball = Instantiate(cannonballPrefab, weaponTransform.position, Quaternion.identity);
             Projectile projectile = cannonball.GetComponent<Projectile>();
+            SoundManager.instance.PlaySoundFXClip(CannonShoot, transform, 1f);
             if (projectile != null)
             {
                 projectile.SetTarget(target.transform);
             }
         }
     }
-
+    //targets the enemy with the highest health within the radius circle that is its firing range.
     protected override Enemy TargetEnemy
     {
         get
