cc7d44f1ad2aa76f29fd9e163396e94902f2b586
Clarkz2263707 <zachary.clark4@g.austincc.edu>
2026-08-09 00:01:26 -0500

    added summary comment.

--- Assets/Scripts/Manager/GameOverManager.cs
+++ Assets/Scripts/Manager/GameOverManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//summary
+//when you get a gameover it allows you to go back to the main menu when you press the button.
+//summary
 public class GameOverManager : MonoBehaviour
 {
     [SerializeField] private string mainMenuSceneName = "MainMenu";
