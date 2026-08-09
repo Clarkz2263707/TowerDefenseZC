e1379aacc20c671aef7b81aae87e9d8067d3d7ca
Clarkz2263707 <zachary.clark4@g.austincc.edu>
2026-08-09 00:06:04 -0500

    added comments that explains what happens within the script.

--- Assets/Scripts/Manager/MoneyManager.cs
+++ Assets/Scripts/Manager/MoneyManager.cs
@@ -1,8 +1,11 @@
 using TMPro;
 using UnityEngine;
-
+//summary
+//Handles the cash within the levels.
+//summary
 public class MoneyManager : MonoBehaviour
 {
+    //establishes starting money
     public static MoneyManager Instance { get; private set; }
 
     [SerializeField] private int startingMoney = 200;
@@ -27,13 +30,13 @@ public class MoneyManager : MonoBehaviour
         currentMoney = startingMoney;
         UpdateMoneyUI();
     }
-
+    //add money method for when an enemy is killed
     public void AddMoney(int amount)
     {
         currentMoney += amount;
         UpdateMoneyUI();
     }
-
+    //spend money method used to build towers in tower script
     public bool SpendMoney(int amount)
     {
         if (currentMoney >= amount)
@@ -44,12 +47,12 @@ public class MoneyManager : MonoBehaviour
         }
         return false;
     }
-
+    //gets current money to be displayed
     public int GetMoney()
     {
         return currentMoney;
     }
-
+    //displays current money.
     private void UpdateMoneyUI()
     {
         if (text != null)
