1be654a08466cbad2ce59a54946878b6ac5c81e7
Clarkz2263707 <zachary.clark4@g.austincc.edu>
2026-08-08 22:59:08 -0500

    added summary text comments to explain what the main menu manager actually does.

--- Assets/Scripts/Manager/MainMenuManager.cs
+++ Assets/Scripts/Manager/MainMenuManager.cs
@@ -6,7 +6,9 @@ public class MainMenuManager1 : MonoBehaviour
     [SerializeField] private string level1 = "Level1";
     [SerializeField] private string optionsMenu = "OptionsMenu";
 
-
+    // Summary
+    //Handles the operations of the main menu in regards to pressing the play button, options button, and the quit button.
+    // Summary
     public void OnStartButtonPressed()
     {
         SceneManager.LoadScene(level1);
